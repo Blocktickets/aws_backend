eee5c969834412d0b9dfd691911663040ad2e1c3
Blocktickets <prashantbisht1997@gmail.com>
2023-06-23 17:01:10 +0530

    sixth commit

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ app.listen(3000, () => console.log("api running on port 3000"))
 
 
 app.get('/', (req, res) => res.json("API is running on aws") )
+
+app.get('/list', (req,res) => res.json("This is the list of all eligible candidates. Kindly take the screenshot before it gets deleted."))
